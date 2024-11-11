3be7f00d287d803f8d657e695c481deb8bf88e8f
mairi tikk <mairi.tikk@gmail.com>
2024-11-11 10:27:32 +0100

    db global

--- src/config/db.mjs
+++ src/config/db.mjs
@@ -2,5 +2,9 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: '127.0,0.1',
     user: 'root',
-    password: 'MySql'
-})
+    password: '6790',
+    port: 3306,
+    database: 'to_to_db'
+});
+
+global.db = pool.promise();
