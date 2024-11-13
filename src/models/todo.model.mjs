16b7383f4d5bccbafffe47f48037f14c62995bb1
mairi tikk <mairi.tikk@gmail.com>
2024-11-13 10:56:58 +0100

    add insertTodo

--- src/models/todo.model.mjs
+++ src/models/todo.model.mjs
@@ -5,9 +5,9 @@ const selectAllTodos = async () => {
     const [rows] = await db.query('SELECT * FROM todo');
     return rows;
 };
-const insertTodo = () => {
-
-
+const insertTodo = ({ to_do }) => {
+    return db.query('INSERT INTO todo(to_do) VALUES()');
 }
+
 
 export default { selectAllTodos, insertTodo };
