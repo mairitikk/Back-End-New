b2c318df57065c854b7062e35efdc29f331fa1e1
mairi tikk <mairi.tikk@gmail.com>
2024-11-13 11:11:18 +0100

    add controller

--- src/controllers/todos.controller.mjs
+++ src/controllers/todos.controller.mjs
@@ -12,18 +12,31 @@ const getAllTodos = async (req, res) => {
     }
 };
 
+const createTodo = async (req, res) => {
+    try {
+        const newTodo = await TodoModel.createTodo(req.body.todo);
+        res.json(newTodo);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
 
+const updateTodo = async (req, res) => {
+    try {
+        const updatedTodo = await TodoModel.updateTodo(req.params.id, req.body);
+        res.json(updatedTodo);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
 
-
-const createTodo = (req, res) => {
-    console.log(req.body)
-    res.send('se crea todo');
-}
-const updateTodo = (req, res) => {
-    res.send('se actualiza todo');
-}
-const deleteTodo = (req, res) => {
-    res.send('se elimina todo');
-}
+const deleteTodo = async (req, res) => {
+    try {
+        await TodoModel.deleteTodo(req.params.id);
+        res.sendStatus(204); // No Content
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
 
 export { getAllTodos, createTodo, updateTodo, deleteTodo };
