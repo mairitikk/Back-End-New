a010254d1aad13b1a6b2943df05ebf6393a9b608
mairi tikk <mairi.tikk@gmail.com>
2024-11-13 21:53:25 +0100

    add user model

---
+++ src/models/user.model.mjs
@@ -0,0 +1,41 @@
+import db from '../config/db.mjs';
+
+const selectAllUsers = async () => {
+    const [rows] = await db.query('SELECT * FROM user');
+    return rows;
+};
+const insertUser = async ({ user }) => {
+    try {
+        const [result] = await db.query('INSERT INTO user(user) VALUES (?)', [user]);
+        const insertId = result.insertId;
+        return insertId;
+    } catch (error) {
+        console.error('Error inserting user:', error);
+        throw error;
+    }
+
+};
+
+const updateUser = async (id, { user }) => {
+    try {
+        const [result] = await db.query('UPDATE user SET user = ? WHERE id = ?', [user, id]);
+        return result.affectedRows > 0;
+    } catch (error) {
+        console.error('Error updating user:', error);
+        throw error;
+    }
+};
+const deleteUser = async (id) => {
+    try {
+        const [result] = await db.query('DELETE FROM user WHERE id = ?', [id]);
+        return result.affectedRows > 0;
+    } catch (error) {
+        console.error('Error deleting user:', error);
+        throw error;
+    }
+};
+
+
+
+
+export default { selectAllUsers, insertUser, updateUser, deleteUser };
