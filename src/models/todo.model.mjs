05edb88532375da25f628625f6bf414330e20c23
mairi tikk <mairi.tikk@gmail.com>
2024-11-13 21:06:02 +0100

    add updateToDo and deleteTodo

--- src/models/todo.model.mjs
+++ src/models/todo.model.mjs
@@ -13,7 +13,29 @@ const insertTodo = async ({ to_do }) => {
         console.error('Error inserting todo:', error);
         throw error;
     }
+
+};
+
+const updateTodo = async ({ id, to_do }) => {
+    try {
+        const [result] = await db.query('UPDATE todo SET todo = ? WHERE id = ?', [to_do, id]);
+        return result.affectedRows > 0;
+    } catch (error) {
+        console.error('Error updating todo:', error);
+        throw error;
+    }
+};
+const deleteTodo = async (id) => {
+    try {
+        const [result] = await db.query('DELETE FROM todo WHERE id = ?', [id]);
+        return result.affectedRows > 0;
+    } catch (error) {
+        console.error('Error deleting todo:', error);
+        throw error;
+    }
 };
 
 
-export default { selectAllTodos, insertTodo };
+
+
+export default { selectAllTodos, insertTodo, updateTodo, deleteTodo };
