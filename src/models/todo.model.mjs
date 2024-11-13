fc5d2f338c244152ccdf405227dd0b9ee8c5b9a7
mairi tikk <mairi.tikk@gmail.com>
2024-11-13 11:06:42 +0100

    insert changes

--- src/models/todo.model.mjs
+++ src/models/todo.model.mjs
@@ -5,9 +5,16 @@ const selectAllTodos = async () => {
     const [rows] = await db.query('SELECT * FROM todo');
     return rows;
 };
-const insertTodo = ({ to_do }) => {
-    return db.query('INSERT INTO todo(to_do) VALUES()');
-}
+const insertTodo = async ({ to_do }) => {
+    try {
+        const [result] = await db.query('INSERT INTO todo(to_do) VALUES (?)', [to_do]);
+        const insertId = result.insertId;
+        return insertId;
+    } catch (error) {
+        console.error('Error inserting todo:', error);
+        throw error;
+    }
+};
 
 
 export default { selectAllTodos, insertTodo };
