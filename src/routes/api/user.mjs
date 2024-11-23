60a55e70126a856737eb7dc20ca144a83264bde7
mairi tikk <mairi.tikk@gmail.com>
2024-11-23 19:09:23 +0100

    add login mjs

--- src/routes/api/user.mjs
+++ src/routes/api/user.mjs
@@ -1,16 +1,16 @@
 
 import express from 'express';
-import { getAllUsers, createUser, updateUser, deleteUser, register, login } from '../../controllers/users.controller.mjs';
+import { getAllUsers, createUser, updateUser, deleteUser, register } from '../../controllers/users.controller.mjs';
 
 const router = express.Router();
 
 
 // Define your todo routes here
 router.get('/', getAllUsers);
-//router.post('/', createUser);
+router.post('/', createUser);
 router.post('/', register);
 
-router.post('', login)
+
 
 router.put('/:userId', updateUser);
 router.delete('/:userId', deleteUser);
