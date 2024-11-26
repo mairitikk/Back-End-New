48ffd139fbf762efae14ed039b8d248879690fed
mairi tikk <mairi.tikk@gmail.com>
2024-11-26 11:45:48 +0100

    deleting login.model

--- src/models/user.model.mjs
+++ src/models/user.model.mjs
@@ -67,5 +67,14 @@ const deleteUser = async (id) => {
         throw error;
     }
 };
+const findByEmail = async (email) => {
+    try {
+        const [rows] = await db.query('SELECT * FROM login WHERE email = ?', [email]);
+        return rows[0];
+    } catch (error) {
+        console.error('Error finding user:', error);
+        throw new Error('Failed to find user');
+    }
+};
 
-export default { selectAllUsers, updateUser, deleteUser };
+export default { selectAllUsers, updateUser, deleteUser, findByEmail };
