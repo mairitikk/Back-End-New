e78910ade1270c49841c009b36e4bb3e27e3f88c
mairi tikk <mairi.tikk@gmail.com>
2024-12-03 11:29:04 +0100

    midellware changes

--- src/middelwares/auth.middleware.mjs
+++ src/middelwares/auth.middleware.mjs
@@ -1,9 +1,11 @@
+import jwt from 'jsonwebtoken';
+
 const checkToken = (req, res, next) => {
     if (!req.headers.authorization) {
         return res.status(403).json({ message: 'you need authorization' });
     }
+    const token = req.headers['authorization'];
 
-    // ... rest of your token validation logic ...
 
     next();
 };
