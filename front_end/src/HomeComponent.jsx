893d6bcf4fb0b793cbf3d260fd2c7f60c34b2103
mairi tikk <mairi.tikk@gmail.com>
2024-12-18 10:59:20 +0100

    changes in HomeComponent

--- front_end/src/HomeComponent.jsx
+++ front_end/src/HomeComponent.jsx
@@ -1,31 +1,34 @@
-<div className={styles.container}>
-    <div className={styles.loginContainer}>
-        <div className="button-container">
+import styles from './styles/HomeComponent.module.css';
+import { NewTodoForm } from "./NewTodoForm";
+import { TodoList } from "./TodoList";
 
-            <button className="btn-r">Sisene</button>
-            <button className="btn-r">Registreeri</button>
-
-
+function HomeComponent({ todos, addTodo, toggleTodo, deleteTodo }) {
+    return (
+        <div className={styles.container}>
+            <div className={styles.loginContainer}>
+                <div className="button-container">
+                    <button className="btn-r">Sisene</button>
+                    <button className="btn-r">Registreeri</button>
+                </div>
+                <div className="form-container">
+                    <NewTodoForm onSubmit={addTodo} />
+                </div>
+            </div>
+            <div>
+                <h1 className="header">
+                    <span>Ü</span>
+                    <span>l</span>
+                    <span>e</span>
+                    <span>s</span>
+                    <span>a</span>
+                    <span>n</span>
+                    <span>d</span>
+                    <span>e</span>
+                    <span>d</span>
+                </h1>
+                <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+            </div>
         </div>
-
-        <div className="form-container">
-            <NewTodoForm onSubmit={addTodo} />
-        </div>
-    </div>
-    <div>
-        <h1 className="header">
-            <span>Ü</span>
-            <span>l</span>
-            <span>e</span>
-            <span>s</span>
-            <span>a</span>
-            <span>n</span>
-            <span>d</span>
-            <span>e</span>
-            <span>d</span>
-        </h1>
-        <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
-    </div>
-
-</div>
-        )
+    );
+}
+export default HomeComponent
