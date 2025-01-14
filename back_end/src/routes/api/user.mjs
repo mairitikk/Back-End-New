837bb7a21abb2ad1a03703aadaaaa63e36b808eb
mairi tikk <mairi.tikk@gmail.com>
2025-01-14 12:26:51 +0100

    add tokken

--- back_end/src/routes/api/user.mjs
+++ back_end/src/routes/api/user.mjs
@@ -3,9 +3,9 @@ import express from 'express';
 import { getAllUsers, updateUser, deleteUser, register, login } from '../../controllers/users.controller.mjs';
 
 const router = express.Router();
+router.use(checkToken);
 
-
-// Define your todo routes here
+// Define your user routes here
 router.get('/', getAllUsers);
 router.post('/register', register);
 router.post('/login', login)
