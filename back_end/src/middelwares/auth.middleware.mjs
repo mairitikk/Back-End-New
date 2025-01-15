d832f3176194a63d17e2f890e3384059aa8b1c0d
mairi tikk <mairi.tikk@gmail.com>
2025-01-15 09:55:08 +0100

    add new auth middleware

--- back_end/src/middelwares/auth.middleware.mjs
+++ back_end/src/middelwares/auth.middleware.mjs
@@ -12,28 +12,25 @@ async function checkToken(req, res, next) {
             return res.status(401).json({ message: 'Unauthorized: No token provided' });
         }
 
-        const token = authHeader.split(' ')[1];
+        const token = authHeader.split(' ')[1]; // Extract token after 'Bearer '
         console.log('Received token:', token);
 
         const decoded = jwt.verify(token, secret);
         console.log('Decoded token:', decoded);
 
-        // Use userModel to find the user based on the decoded userId
-        const user = await userModel.findById(decoded.userId);
-
-        if (!user) {
-            console.error('User not found');
-            return res.status(401).json({ message: 'Unauthorized: User not found' });
-        }
-
-        // Attach user information to the request object (optional)
-        req.user = user;
+        req.userId = decoded.userId;
 
         next();
     } catch (error) {
         console.error('Authentication error:', error);
         console.log('Error message:', error.message);
-        return res.status(403).json({ message: 'Forbidden' });
+        if (error.name === 'JsonWebTokenError') {
+            return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+        } else if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({ message: 'Unauthorized: Token expired' });
+        } else {
+            return res.status(500).json({ message: 'Internal Server Error' });
+        }
     }
 }
 
