41ea6d859b466b4446adb2f3d0c6fb49b0d135de
mairi tikk <mairi.tikk@gmail.com>
2025-01-15 10:04:53 +0100

    app login changes

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './Login';
+
 import Registration from './Registration';
 import Home from './Home';
+import Login from './login';
 
 
 function App() {
