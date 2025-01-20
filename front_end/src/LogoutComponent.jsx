3e8c7c3abaefa5445cb1e2385585f96acf1fdce0
mairi tikk <mairi.tikk@gmail.com>
2025-01-20 09:26:09 +0100

    add logout function

--- front_end/src/LogoutComponent.jsx
+++ front_end/src/LogoutComponent.jsx
@@ -1,1 +1,21 @@
+import { useNavigate } from "react-router-dom"
 import styles from "./styles/LogoutComponent.module.css"
+
+export default function Logout() {
+
+    const navigate = useNavigate()
+
+    async function logout() {
+        localStorage.removeItem("TOKEN")
+        navigate('/')
+    }
+
+
+
+    return (
+
+
+        <button type="button" onClick={logout}>Logi välja</button>
+    )
+
+}
