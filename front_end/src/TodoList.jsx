7942cd2e0e612a797656922f7d1c1aacf2541ab6
mairi tikk <mairi.tikk@gmail.com>
2025-01-21 08:45:42 +0100

    todolist changes

--- front_end/src/TodoList.jsx
+++ front_end/src/TodoList.jsx
@@ -1,24 +1,48 @@
 /* eslint-disable react/prop-types */
-import styles from "./styles/HomeComponent.module.css"
-import { TodoItem } from "./TodoItem"
-import { useState, useEffect } from "react"
+import styles from "./styles/HomeComponent.module.css";
+import { TodoItem } from "./TodoItem";
+import { useState, useEffect } from "react";
 
-export function TodoList({ todos, toggleTodo, deleteTodo }) {
+export function TodoList({ id, toggleTodo, deleteTodo }) {
+  const [todos, setTodos] = useState([]);
+
+  // Fetch todo list from storage based on id on component mount
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const storedTodos = await loadTodosFromStorage(id);
+      setTodos(storedTodos);
+    };
+    fetchTodos();
+  }, [id]);
+
+  const saveTodos = async (newTodos) => {
+    await storeTodosInStorage(id, newTodos);
+    setTodos(newTodos);
+  };
+
+  function loadTodosFromStorage(id) {
+    const storedTodos = localStorage.getItem(`todos-${id}`);
+    if (storedTodos) {
+      return JSON.parse(storedTodos);
+    }
+    return [];
+  }
+
+  function storeTodosInStorage(id, todos) {
+    localStorage.setItem(`todos-${id}`, JSON.stringify(todos));
+  }
+
   return (
-
-
     <ul className={styles.list}>
       {todos.length === 0 && "Pole ülessandeid"}
-      {todos.map(todo => {
-        return (
-          <TodoItem
-            {...todo}
-            key={todo.id}
-            toggleTodo={toggleTodo}
-            deleteTodo={deleteTodo}
-          />
-        )
-      })}
+      {todos.map((todo) => (
+        <TodoItem
+          {...todo}
+          key={todo.id}
+          toggleTodo={toggleTodo}
+          deleteTodo={deleteTodo}
+        />
+      ))}
     </ul>
-  )
+  );
 }
