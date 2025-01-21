e5aecdf8b07ee8ee196f54e99a39c842900877a1
mairi tikk <mairi.tikk@gmail.com>
2025-01-21 08:37:21 +0100

    add use state for todolist

--- front_end/src/TodoList.jsx
+++ front_end/src/TodoList.jsx
@@ -1,9 +1,12 @@
 /* eslint-disable react/prop-types */
 import styles from "./styles/HomeComponent.module.css"
 import { TodoItem } from "./TodoItem"
+import { useState, useEffect } from "react"
 
 export function TodoList({ todos, toggleTodo, deleteTodo }) {
   return (
+
+
     <ul className={styles.list}>
       {todos.length === 0 && "Pole ülessandeid"}
       {todos.map(todo => {
