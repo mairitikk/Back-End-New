22e5747fbc64d2bbf77f16bffd582e035bf31452
mairi tikk <mairi.tikk@gmail.com>
2025-01-23 10:33:10 +0100

    token changes

--- back_end/src/middelwares/auth.middleware.mjs
+++ back_end/src/middelwares/auth.middleware.mjs
@@ -5,14 +5,19 @@ const secret = process.env.JWT_SECRET;
 async function checkToken(req, res, next) {
     try {
         const authHeader = req.headers.authorization;
-        console.log('Authorization header:', authHeader)
+        console.log('Authorization header:', authHeader);
+
         if (!authHeader || !authHeader.startsWith('Bearer ')) {
             return res.status(401).json({ message: 'Unauthorized: No token provided' });
         }
 
         const token = authHeader.split(' ')[1];
-
         const decoded = jwt.verify(token, secret);
+
+        // Check if userId exists in the decoded payload
+        if (!decoded.userId) {
+            return res.status(401).json({ message: 'Unauthorized: Invalid token payload' });
+        }
 
         req.userId = decoded.userId;
 
